92eaca370156e694b2b08f836bde35b56bc415de
Leirach <juandleon27@gmail.com>
2019-10-31 09:43:03 -0600

    Initial Commit

---
+++ Acceso NIC prototipo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Acceso NIC prototipo
+//
+//  Created by Juan De Leon on 10/31/19.
+//  Copyright © 2019 Juan De Leon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
